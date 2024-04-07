719f4f73faae2a6053305bc6e287c70a2c5acf5c
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-07 22:55:26 +0200

    retrieving and filtering objects in django

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from store.models import Product
 
 def say_hello(request):
-    x = 1
-    y = 2
-    return render(request, 'hello.html', {'name': 'peter'})
+    products = Product.objects.filter(title__icontains='lemon')
+    return render(request, 'hello.html', {'name': 'peter', 'products': list(products)})
--- storefront/settings.py
+++ storefront/settings.py
@@ -85,8 +85,15 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES'
+        }
     }
 }
 
