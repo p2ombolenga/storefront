174a42039bef8a1a8349c2c490c1ace898e445ef
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-14 23:21:53 +0200

    understand django ORM

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
-from store.models import Product
+from django.db import transaction
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product,Collection,Order,OrderItem
+from tags.models import TaggedItem
+
 
 def say_hello(request):
-    products = Product.objects.filter(title__icontains='lemon')
-    return render(request, 'hello.html', {'name': 'peter', 'products': list(products)})
+    # #custom manager
+    # query_set = TaggedItem.objects.get_tags_for(Product, 500)
+    
+    # #creating objects
+    # collection = Collection()
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
+    
+    ## update 
+    # Collection.objects.filter(pk=6).update(featured_product=None)
+
+
+    ## deleting objects
+    # collection = Collection(pk=7)
+    # collection.delete()
+
+    ## using transaction
+    # with transaction.atomic():
+    #     order = Order()
+    #     order.customer_id = 1
+    #     order.save()
+
+    #     item = OrderItem()
+    #     item.order = order
+    #     item.product_id = -1
+    #     item.quantity = 1
+    #     item.unit_price = 10
+    #     item.save()
+
+
+
+    return render(request, 'hello.html', { 'name': 'peter'})
