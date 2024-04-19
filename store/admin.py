09a9aec9b01c11f38bbc037c434908b72f9fe00d
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-19 18:53:55 +0200

    overriding base queryset and providing links to other pages

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from .models import Collection, Product, Customer, Order
 
 @admin.register(Product)
@@ -19,11 +22,24 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name','last_name']
     list_per_page = 25
 
+    @admin.display(ordering='orders_count')
+    def orders_count(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({'customer__id': str(customer.id)})) 
+        return format_html('<a href={}>{}</a>', url, customer.orders_count)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+    
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['placed_at', 'payment_status', 'customer_name']
@@ -33,4 +49,19 @@ class OrderAdmin(admin.ModelAdmin):
     def customer_name(self, order):
         return order.customer
     
-admin.site.register(Collection)
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (
+            reverse('admin:store_product_changelist') 
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url,collection.products_count)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count('product'))
